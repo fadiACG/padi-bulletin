55517fbcae4465f9f6172992e8905ca276d48216
fadiACG <91090473+fadiACG@users.noreply.github.com>
2022-07-16 18:42:57 +0300

    Update settings.py

--- app/padi_bulletin/settings.py
+++ app/padi_bulletin/settings.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -115,3 +117,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
